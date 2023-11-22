c0f8c6676d3aee6c5ed8063a9989a3fb903950c3
Sami Al-Dury <git@aldury.com>
2023-11-22 21:21:25 +0100

    fix: eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -4,8 +4,10 @@ export default [
     ...eslint.config({
         node: true,
         typeScript: true,
+    }),
+    {
         rules: {
             'no-console': 'off',
         },
-    }),
+    },
 ]
